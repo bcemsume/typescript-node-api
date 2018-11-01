8c0d36633a491d18aaa01a1f92d82d1b6f9302cb
Cem SUME <RDDeveloper.erdem.yildiz@drd.com.tr>
2018-11-01 17:57:24 +0300

    postgre sequelize integration

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser'; //use to parse the form data that you pass in the request
 import { Pokemons } from "./routes/pokemons";
+import  { Sequelize } from 'sequelize-typescript';
+
+
+const sequelize = new Sequelize({
+    database: 'd1dkamedibdn97',
+    dialect: 'postgres',
+    username: 'vegrltaargvijh',
+    password: '18d9c26aa79b4fee28366097890db749bd58527f59e22ec179da875854e9b4b8',
+    storage: ':memory:',
+    modelPaths: [__dirname + '/models']
+});;
 
 class App {
+
 
     public app: express.Application;
 
